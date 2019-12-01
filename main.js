96b62a0b301f1513cb6d66b9eb731d59de037031
Jarred Glaser <jglaser@continuus-technologies.com>
2019-12-01 12:30:17 -0600

    fixed nav bar sizing issue

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 // TODO: https://www.developerdrive.com/full-page-hero-image-html-css/
+
+function showScreenWidth() {
+    let w = window.innerWidth;
+    let h = window.innerHeight;
+    console.log(`Size: ${w} x ${h}`)
+}
