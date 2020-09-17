3b58ef6310a87673cc01003157372afd87ef3271
jdglaser <jarred.glaser@gmail.com>
2020-09-17 11:35:36 -0500

    change everything

--- main.js
+++
@@ -1,7 +0,0 @@
-// TODO: https://www.developerdrive.com/full-page-hero-image-html-css/
-
-function showScreenWidth() {
-    let w = window.innerWidth;
-    let h = window.innerHeight;
-    console.log(`Size: ${w} x ${h}`)
-}
